0b1851a6d57b604899221679445106efe99606a8
phamvanoanh <oanhmrphamvan@gmail.com>
2018-04-10 23:34:04 +0700

    Add test of CustomerController

--- src/main/java/com/springboot/edu/domain/Customer.java
+++ src/main/java/com/springboot/edu/domain/Customer.java
@@ -15,8 +15,48 @@ public class Customer {
   private String firstName;
   private String lastName;
 
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
-  private LocalDate BirthDate;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+  private LocalDate birthDate;
 
   private Status status;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
 }
--- src/test/java/com/springboot/edu/controller/CustomerControllerTest.java
+++ src/test/java/com/springboot/edu/controller/CustomerControllerTest.java
@@ -1,16 +1,25 @@
 package com.springboot.edu.controller;
 
 import com.springboot.edu.domain.Customer;
+import com.springboot.edu.domain.Status;
 import com.springboot.edu.service.CustomerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = CustomerController.class)
@@ -25,7 +34,23 @@ public class CustomerControllerTest {
   @Test
   public void test_customer_can_be_serialized_correctly() throws Exception {
     Customer customer = new Customer();
-    customer.
+    customer.setBirthDate(LocalDate.of(2018, 4, 10));
+    customer.setFirstName("pv");
+    customer.setLastName("oanh");
+    customer.setStatus(Status.VIP);
+
+    given(customerService.findAllCustomers()).willReturn(Arrays.asList(customer));
+    mockMvc.perform(get("/customers")
+            .accept(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(content().json("[\n" +
+              "  {\n" +
+              "    \"firstName\" : \"pv\",\n" +
+              "    \"lastName\" : \"oanh\",\n" +
+              "    \"status\" : \"VIP\",\n" +
+              "    \"birthDate\" :\"2018-04-10\"\n" +
+              "  }\n" +
+              "]\n"));
   }
 
 }
